4e5bee999b5ada5e0eacb1212a6aaba9c1890558
istone <istoneyou1205@yahoo.co.jp>
2023-08-03 11:32:45 +0900

    ansible-playbook-configure-ec2-monitoringの追加

--- ansible-playbook-configure-ec2-monitoring.tf
+++ ansible-playbook-configure-ec2-monitoring.tf
@@ -1,8 +1,8 @@
-module "ansible_playbook_ec2_monitoring_pipeline" {
+module "ansible_playbook_configure_ec2_monitoring_pipeline" {
   source = "./modules/pipeline_ansible"
 
-  project_name                 = "ansible-playbook-ec2-monitoring"
-  codecommit_repo_name         = "ansible-playbook-ec2-monitoring"
+  project_name                 = "ansible-playbook-configure-ec2-monitoring"
+  codecommit_repo_name         = "ansible-playbook-configure-ec2-monitoring"
   region                       = var.region
   artifacts_bucket             = var.artifacts_bucket_name
   artifacts_bucket_key_arn     = var.artifacts_bucket_key_arn
---
+++ ansible-playbook-install-ec2-monitoring.tf
@@ -0,0 +1,10 @@
+module "ansible_playbook_install_ec2_monitoring_pipeline" {
+  source = "./modules/pipeline_ansible"
+
+  project_name                 = "ansible-playbook-install-ec2-monitoring"
+  codecommit_repo_name         = "ansible-playbook-install-ec2-monitoring"
+  region                       = var.region
+  artifacts_bucket             = var.artifacts_bucket_name
+  artifacts_bucket_key_arn     = var.artifacts_bucket_key_arn
+  ansible_playbook_bucket_name = var.ansible_playbook_bucket_name
+}
